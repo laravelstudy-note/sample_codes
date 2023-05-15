6910bac2bc12d16748ca41751880aa05e6cdc8bb
miyazawa <ryosukemiyazawa@gmail.com>
2023-05-15 09:28:00 +0900

    途中まで

--- sample-app/src/Detail.tsx
+++ sample-app/src/Detail.tsx
@@ -1,6 +1,17 @@
 import { useParams } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import { useEffect, useState } from "react";
+import {
+	Breadcrumbs,
+	Link,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	Typography,
+} from "@mui/material";
+
+import {AccountCircle, Cake} from "@mui/icons-material";
 
 type PersonalInformation = {
 	index: number;
@@ -15,12 +26,11 @@ type State = {
 	item?: PersonalInformation;
 };
 
-export default function Detail(){
-
+export default function Detail() {
 	const { id } = useParams();
 
 	const [state, setState] = useState<State>({});
-	
+
 	const loadJson = async () => {
 		const result = await fetch("/json/personal_information.json");
 
@@ -30,8 +40,8 @@ export default function Detail(){
 		const items = personal_infomations["personal-infomation"] as any[];
 
 		const item = items.find((item) => item["連番"] == id);
-		if(!item){
-			alert("データの読み込みに失敗しました")
+		if (!item) {
+			alert("データの読み込みに失敗しました");
 			return;
 		}
 
@@ -44,7 +54,7 @@ export default function Detail(){
 				gender: item["性別"] as string,
 				telephone: item["電話番号"] as string,
 				birthday: item["生年月日"] as string,
-			}
+			},
 		});
 	};
 
@@ -56,13 +66,33 @@ export default function Detail(){
 		<Layout>
 			<h1>詳細画面(#{id})</h1>
 
-			{ state.item && <PersonalInformationTable item={ state.item } />}
+			<Breadcrumbs>
+				<Link href="/">TOP</Link>
+				<Link href="/">一覧画面</Link>
+				<Typography>詳細画面</Typography>
+			</Breadcrumbs>
+
+			{state.item && <PersonalInformationTable item={state.item} />}
 		</Layout>
-	)
+	);
 }
 
-function PersonalInformationTable({ item } : { item: PersonalInformation }){
+function PersonalInformationTable({ item }: { item: PersonalInformation }) {
 	return (
-		<p>aaa</p>
-	)
+		<List>
+			<ListItem>
+					<ListItemIcon>
+						<AccountCircle />
+					</ListItemIcon>
+					<ListItemText primary={item.name} secondary={item.reading} />
+			</ListItem>
+
+			<ListItem>
+					<ListItemIcon>
+						<Cake />
+					</ListItemIcon>
+					<ListItemText primary={item.gender} secondary={item.birthday}/>
+			</ListItem>
+		</List>
+	);
 }
